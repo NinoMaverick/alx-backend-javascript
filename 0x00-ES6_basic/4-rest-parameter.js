46b2d752b402144152a2941f94378d35b29cbf73
NinoMaverick <etengyemmy@gmail.com>
2024-08-30 17:09:16 +0100

    checks

---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,5 @@
+export default function returnHowManyArguments(...args) {
+    // 'args' is now an array containing all the arguments passed to the function.
+    return args.length; // Returns the number of arguments
+}
+
