c62993d8cecb72b7918905d7636a0f40883b2ceb
NinoMaverick <etengyemmy@gmail.com>
2024-09-03 09:52:06 +0100

    tasks

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function appendToEachArrayValue(array, appendString) {
+    const newArray = [];
+    for (const value of array) {
+      newArray.push(appendString + value);
+    }
+  
+    return newArray;
+  }
+  
---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function createReportObject(employeesList) {
+    return {
+      allEmployees: employeesList,
+      getNumberOfDepartments(employeesList) {
+        return Object.keys(employeesList).length;
+      },
+    };
+  }
+  
